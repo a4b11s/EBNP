b54ff4b9a2d5f550155f03e3ebbed7c871aa8d98
Artem <a4b11s@gmail.com>
2024-09-06 19:45:32 +0300

    Refactor code structure and remove unused event submission method

--- ebnp/enums/methods.py
+++ ebnp/enums/methods.py
@@ -1,12 +1,5 @@
-# - SUB: subscribe to a specific event
-# - UNSUB: unsubscribe from a specific event
-# - EMIT: emit a specific event
-# - CHECK: check if a specific event has been emitted
-# - ADD: add an event for submission
-
-
 class Methods:
-    
+
     SUB = "SUB"
     UNSUB = "UNSUB"
     EMIT = "EMIT"
