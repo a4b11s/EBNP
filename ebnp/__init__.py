f1bd942c41b9e7abd86ed5a39ad602153aac1814
Artem <a4b11s@gmail.com>
2024-09-06 19:44:53 +0300

    Refactor code structure and add event transfer protocol documentation

--- ebnp/__init__.py
+++ ebnp/__init__.py
@@ -4,13 +4,14 @@
 """
 
 from .modules import event
-from .enums import args_type
+from .enums import args_type, methods
 
 from .modules.event import Event
 from .enums.args_type import TYPES_ENUM
+from .enums.methods import Methods
 
 
 __author__ = "Artem Bazyl"
 __license__ = "MIT"
 __version__ = "0.0.1"
-__all__ = ["event", "args_type", "Event", "TYPES_ENUM"]
+__all__ = ["event", "args_type", "Event", "TYPES_ENUM", "methods", "Methods"]
---
+++ ebnp/enums/methods.py
@@ -0,0 +1,14 @@
+# - SUB: subscribe to a specific event
+# - UNSUB: unsubscribe from a specific event
+# - EMIT: emit a specific event
+# - CHECK: check if a specific event has been emitted
+# - ADD: add an event for submission
+
+
+class Methods:
+    
+    SUB = "SUB"
+    UNSUB = "UNSUB"
+    EMIT = "EMIT"
+    CHECK = "CHECK"
+    ADD = "ADD"
